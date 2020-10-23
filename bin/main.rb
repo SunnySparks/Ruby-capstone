89c401462d35e33fb52a2f3c7f03231734afc624
SunnySparks <josediazt6@outlook.com>
2020-10-23 16:10:19 -0500

    fix rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,25 @@
 #!/usr/bin/ruby
 require './lib/linter.rb'
 
-#This code should trigger errors
+# This code should trigger errors
 error_trigger = ClosingParenthesis.new('./file.rb')
 error_trigger.closed_check('./file.rb')
 
 if error_trigger.les_erreurs.empty?
-  puts "No offenses detected in", error_trigger.display_path
+  puts 'No offenses detected in', error_trigger.display_path
 else
-  puts "File: ", error_trigger.display_path
+  puts 'File: ', error_trigger.display_path
   puts error_trigger.trigger_errors
 end
 
-#This code shouldn't trigger any error
+# This code shouldn't trigger any error
 
 right_syntax = ClosingParenthesis.new('./correct_syntax.rb')
 right_syntax.closed_check('./correct_syntax.rb')
 
 if right_syntax.les_erreurs.empty?
-  print "No offenses detected in", right_syntax.display_path
+  print 'No offenses detected in', right_syntax.display_path
 else
-  puts "File: ", right_syntax.display_path
+  puts 'File: ', right_syntax.display_path
   puts right_syntax.trigger_errors
 end
--- correct_syntax.rb
+++ correct_syntax.rb
@@ -1,12 +1,12 @@
 class CorrectSyntax
-    atrr_reader :array, :hash
-    def initialize
-        @array = []
-        @hash
-    end
+  atrr_reader :array, :hash
+  def initialize
+    @array = []
+    @hash = {}
+  end
 
-    def correct
-        @array = [1, 5, 22, "a", true, "happy"]
-        @hash = { name: "Bunny", birthday: "Jun 30th", blood_type: "o"}
-    end
+  def correct
+    @array = [1, 5, 22, 'a', true, 'happy']
+    @hash = { name: 'Bunny', birthday: 'Jun 30th', blood_type: 'o' }
+  end
 end
