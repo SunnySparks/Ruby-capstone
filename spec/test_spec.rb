4dad55c56eaa5fcc7bb1f464f84761c79a93aee2
SunnySparks <josediazt6@outlook.com>
2020-10-26 16:58:38 -0600

    Update tests of rspec

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -13,6 +13,7 @@ describe '#ClosingParenthesis' do
   end
 
   describe '#trigger_errors' do
+  let(:les_erreurs) { ClosingParenthesis.new }
     it 'It stores the value of "[" in the first key of a hash when a missing parenthesis is found' do
       expect do
         expect(erreurs['[']).to eql(0)
@@ -22,6 +23,18 @@ describe '#ClosingParenthesis' do
     it 'It stores the value of "(" in the second key of a hash when a missing parenthesis is found' do
       expect do
         expect(erreurs['(']).to eql(1)
+      end
+    end
+
+    it 'when there is at least one missing parenthesis' do
+      expect do
+        (les_erreurs).not_to be_empty
+      end
+    end
+
+    it 'generates an error when expected', negative: true do 
+      expect do
+        (les_erreurs).not_to eql(errors) 
       end
     end
   end
