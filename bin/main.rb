d948a4b25aca02ff4f75a78f5b2291e6aa20c427
SunnySparks <josediazt6@outlook.com>
2020-11-03 08:02:53 -0600

    Add changes to linter.rb

--- bin/main.rb
+++ bin/main.rb
@@ -3,11 +3,15 @@ require_relative '../lib/linter'
 
 # This code should trigger errors
 error_trigger = ClosingParenthesis.new(ARGV.first)
-error_trigger.closed_check(ARGV.first)
+error_trigger.couples_add(ARGV.first)
+#error_trigger.trigger_parentheses
+#error_trigger.closed_check(ARGV.first)
 
-if error_trigger.les_erreurs.empty?
+=begin
+if error_trigger.errors.empty?
   puts 'File name: ', error_trigger.display_path, 'No offenses detected'
 else
   puts 'File name: ', error_trigger.display_path
-  puts error_trigger.trigger_errors
+  puts error_trigger.puts_errors
 end
+=end
--- lib/linter.rb
+++ lib/linter.rb
@@ -2,58 +2,252 @@
 
 class ClosingParenthesis
   attr_reader :file_path
-  attr_accessor :all_braces, :errors, :erreurs, :les_erreurs, :les_lignes
+  attr_accessor :all_braces, :errors, :erreurs, :les_erreurs, :les_lignes, :opened, :closed, :parentheses, :curly_braces, :square_brackets, :parentheses_open, :parentheses_closed, :total_parentheses
   def initialize(file_path)
     @errors = []
     @erreurs = {}
     @file_path = file_path
     @les_erreurs = []
+    @opened = []
+    @closed = []
+    @parentheses = []
+    @parentheses_open = []
+    @parentheses_closed = []
+    @total_parentheses
+    @pr = []
+    @square_brackets = []
+    @curly_braces = []
     @les_lignes = []
   end
 
-  def closed_check(file_path)
+  def couples_add(file_path)
+    @par_open = 0
+    @par_closed = 0
+    @curly_open = 0
+    @curly_closed = 0
+    @square_open = 0
+    @square_closed = 0
     File.open(file_path).each_with_index do |element, line|
       element.length.times do |i|
         @linea = line + 1
         case element[i]
         when '('
-          @element_p = @les_erreurs.unshift([element[i], @linea.to_s])
-          @open_p = @les_erreurs[0]
-          @index_p = @les_erreurs.index(@open_p)
+          @open_p = @parentheses_open.push([element[i], @linea.to_s])
         when ')'
-          @num_p = @index_p.to_i
-          if !@les_erreurs.empty? && @element_p[0][0].include?('(')
-            @delete_p = @les_erreurs.delete_at(@num_p)
-          else
-            seek_and_destroy('(')
-          end
+          @closed_p = @parentheses_closed.push([element[i], @linea.to_s])
         when '{'
-          @element_curly = @les_erreurs.unshift([element[i], @linea.to_s])
-          @open_curly = @les_erreurs[0]
-          @index_curly = @les_erreurs.index(@open_curly)
+          @open_curly = @curly_braces.push([element[i], @linea.to_s])
         when '}'
-          @num_curly = @index_curly.to_i
-          if !@les_erreurs.empty? && @element_curly[0][0].include?('{')
-            @delete_curly = @les_erreurs.delete_at(@num_curly)
-          else
-            seek_and_destroy('{')
-          end
+          @closed_curly = @curly_braces.push([element[i], @linea.to_s])
+          @curly_c
         when '['
-          @element_b = @les_erreurs.unshift([element[i], @linea.to_s])
-          @open_b = @les_erreurs[0]
-          @index_b = @les_erreurs.index(@open_b)
+          @open_b = @square_brackets.push([element[i], @linea.to_s])
         when ']'
-          @num_b = @index_b.to_i
-          if !@les_erreurs.empty? && @element_b[0][0].include?('[')
-            @delete_b = @les_erreurs.delete_at(@num_b)
-          else
-            seek_and_destroy('[')
+          @closed_p = @square_brackets.push([element[i], @linea.to_s])
+        end
+      end
+    end
+    parentheses_check
+    #trigger_parentheses
+    #trigger_curly
+    #trigger_square
+    #triggerer_of_parentheses
+    #triggerer_of_curly
+    #triggerer_of_squares
+  end
+
+  def parentheses_check
+    @total_parentheses = @parentheses_open.zip @parentheses_closed
+    @total_parentheses.each do |el|
+      #el.length.times do |i, num|
+      #p el
+      #p el[0]
+      #p el[1]
+        #el.length.times do |i, num|
+        if el[1] != nil
+        if el[0][1] != el [1][1] && el[0][0] != el[1][0]
+          @errors.push(el[0])
+        end
+        else
+          @errors.push(el[0]) 
+      end
+      #nd
+        #if el[1] == nil
+        #  puts "hello"
+        #  puts el[1]
+        #end
+        #p el[i][1]
+        #p el[i+1][1]
+        #p "num #{el[i + 1]}"
+        #if el[i][1] == nil
+          #puts "hello #{el[num]}"
+        #end
+      #end
+    end
+    puts @errors
+  end
+=begin
+  def trigger_parentheses
+    if @par_open > @par_closed
+      filter_p_open
+    else
+      filter_p_closed
+    end
+  end
+
+  def filter_p_open
+    @parentheses.each do |el|
+      el.length.times do |i, num|
+        if el[i].include?('(')
+          deletion_left = @parentheses.delete(el)
+        end
+      end
+    end
+  end
+
+  def filter_p_closed
+    @parentheses.each do |el|
+      el.length.times do |i, num|
+        if el[i].include?(')')
+          deletion_right = @parentheses.delete(el)
+        end
+      end
+    end
+  end
+
+  def trigger_curly
+    if @curly_open > @curly_closed
+      filter_curly_open
+    else
+      filter_curly_closed
+    end
+  end
+
+  def filter_curly_open
+    @parentheses.each do |el|
+      el.length.times do |i, num|
+        if el[i].include?('{')
+          deletion_left = @parentheses.delete(el)
+        end
+      end
+    end
+  end
+
+  def filter_curly_closed
+    @parentheses.each_with_index do |el|
+      el.length.times do |i, num|
+        if el[i].include?('}')
+          deletion_right = @parentheses.delete(el)
+        end
+      end
+    end
+  end
+
+  def trigger_square
+    if @square_open > @square_closed
+      filter_square_open
+    else
+      filter_square_closed
+    end
+  end
+
+  def filter_square_open
+    @parentheses.each_with_index do |el|
+      el.length.times do |i, num|
+        if el[i].include?('[')
+          deletion_left = @parentheses.delete(@parentheses[el])
+        end
+      end
+    end
+  end
+
+  def filter_square_closed
+    @parentheses.each_with_index do |el|
+      el.length.times do |i, num|
+        if el[i].include?(']')
+          deletion_right = @parentheses.delete(el)
+        end
+      end
+    end
+  end
+
+
+  def triggerer_of_parentheses
+    unless @parentheses.empty?
+      @parentheses.each do |el|
+        el.length.times do |i|
+          if el[i] == '('
+            elm = ')'
+            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
+          elsif el[i] == ')'
+            elm = '('
+            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
           end
         end
       end
     end
   end
 
+  def triggerer_of_curly
+    unless @curly_braces.empty?
+      @curly_braces.each do |el|
+        el.length.times do |i|
+          if el[i] == '{'
+            elm = '}'
+            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
+          elsif el[i] == '}'
+            elm = '{'
+            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
+          end
+        end
+      end
+    end
+  end
+
+  def triggerer_of_squares
+    unless @square_brackets.empty?
+      @square_brackets.each do |el|
+        el.length.times do |i|
+          if el[i] == '['
+            elm = ']'
+            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
+          elsif el[i] == ']'
+            elm = '['
+            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
+          end
+        end
+      end
+    end
+  end
+=end
+  def puts_errors
+    @errors
+  end
+=begin
+  def parentheses_add(file_path)
+    File.open(file_path).each_with_index do |element, line|
+      element.length.times do |i|
+        @linea = line + 1
+        case element[i]
+        when '('
+          @open_p = @opened.unshift([element[i], @linea.to_s])
+        when ')'
+          @closed_p = @closed.unshift([element[i], @linea.to_s])
+        when '{'
+          @open_curly = @les_erreurs.unshift([element[i], @linea.to_s])
+        when '}'
+          @closed_curly = @closed.unshift([element[i], @linea.to_s])
+        when '['
+          @open_b = @les_erreurs.unshift([element[i], @linea.to_s])
+        when ']'
+          @closed_p = @closed.unshift([element[i], @linea.to_s])
+        end
+      end
+    end
+  end
+=end
+
+=begin
   def trigger_errors
     unless @les_erreurs.empty?
       @les_erreurs.each do |el|
@@ -74,9 +268,7 @@ class ClosingParenthesis
     @errors
   end
 
-  def display_path
-    @file_path
-  end
+
 
   def seek_and_destroy(element)
     @les_erreurs.length.times do |j|
@@ -86,4 +278,8 @@ class ClosingParenthesis
       end
     end
   end
+=end
+def display_path
+  @file_path
+end
 end
