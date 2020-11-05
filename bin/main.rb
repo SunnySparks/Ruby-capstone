d811afdde68867824e7956243544436128046779
SunnySparks <josediazt6@outlook.com>
2020-11-05 16:00:23 -0600

    Fix Rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -4,9 +4,8 @@ require_relative '../lib/linter'
 # This code should trigger errors
 error_trigger = ClosingParenthesis.new(ARGV.first)
 error_trigger.couples_add(ARGV.first)
-#error_trigger.trigger_parentheses
-#error_trigger.closed_check(ARGV.first)
-
+# error_trigger.trigger_parentheses
+# error_trigger.closed_check(ARGV.first)
 
 if error_trigger.errors.empty?
   puts 'File name: ', error_trigger.display_path, 'No offenses detected'
@@ -14,4 +13,3 @@ else
   puts 'File name: ', error_trigger.display_path
   puts error_trigger.puts_errors
 end
-
--- lib/linter.rb
+++ lib/linter.rb
@@ -47,73 +47,46 @@ class ClosingParenthesis
     square_brackets_check
     sorting_errors
     trigger_errors
-    #trigger_parentheses
-    #trigger_curly
-    #trigger_square
-    #triggerer_of_parentheses
-    #triggerer_of_curly
-    #triggerer_of_squares
   end
 
   def parentheses_check
-    for i in 1..@parentheses.length-1 do
-      if @parentheses[i-1][0] == ")" && @parentheses[i][0] == ")"
-        @errors.unshift(@parentheses[i-1])
-      elsif @parentheses[i-1][0] == "(" && @parentheses[i][0] == "("
+    for i in 1..@parentheses.length - 1 do
+      if @parentheses[i - 1][0] == ')' && @parentheses[i][0] == ')'
+        @errors.unshift(@parentheses[i - 1])
+      elsif @parentheses[i - 1][0] == '(' && @parentheses[i][0] == '('
         @errors.unshift(@parentheses[i])
-      elsif @parentheses[i-1][0] == "}" && @parentheses[i][0] == "{" && @parentheses[i-1][1] != @parentheses[i][1]
-        @errors.unshift(@parentheses[i-1])
-      elsif @parentheses[i-1][0] == "{" && @parentheses[i][0] == "}" && @parentheses[i-1][1] == @parentheses[i][1]
-        @errors.unshift(@parentheses[i-1])
+      elsif @parentheses[i - 1][0] == '}' && @parentheses[i][0] == '{' && @parentheses[i - 1][1] != @parentheses[i][1]
+        @errors.unshift(@parentheses[i - 1])
+      elsif @parentheses[i - 1][0] == '{' && @parentheses[i][0] == '}' && @parentheses[i - 1][1] == @parentheses[i][1]
+        @errors.unshift(@parentheses[i - 1])
       end
-=begin
-      p @parentheses[i-1][0]
-      p @parentheses[i][0]
-      if @parentheses[i-1][0] == "("
-        if @parentheses[i][0] == "("
-          #puts @parentheses[i-1][1]
-          #puts @parentheses[i][1]
-        puts "Repeated in line #{@parentheses[i-1][1]}"
-        #puts "they are different #{@parentheses[i][0]}, line #{@parentheses[i][1]}"
-        #if  @parentheses[i-1][1] == @parentheses[i][1]
-        #  puts "same line #{@parentheses[i][1]}"
-        #end
-        @errors.push(@parentheses[i-1])
-        end
-      elsif @parentheses[i-1][0] == ")"
-        if @parentheses[i][0] == ")"
-          puts "Repeated in line #{@parentheses[i-1][1]}"
-          @errors.push(@parentheses[i-1])
-        end
-      end
-=end
     end
   end
 
   def curly_braces_check
-    for i in 1..@curly_braces.length-1 do
-      if @curly_braces[i-1][0] == "}" && @curly_braces[i][0] == "}"
-        @errors.unshift(@curly_braces[i-1])
-      elsif @curly_braces[i-1][0] == "{" && @curly_braces[i][0] == "{"
+    for i in 1..@curly_braces.length - 1 do
+      if @curly_braces[i - 1][0] == '}' && @curly_braces[i][0] == '}'
+        @errors.unshift(@curly_braces[i - 1])
+      elsif @curly_braces[i - 1][0] == '{' && @curly_braces[i][0] == '{'
         @errors.unshift(@curly_braces[i])
-      elsif @curly_braces[i-1][0] == "}" && @curly_braces[i][0] == "{" && @curly_braces[i-1][1] != @curly_braces[i][1]
-      @errors.unshift(@curly_braces[i-1])
-      elsif @curly_braces[i-1][0] == "{" && @curly_braces[i][0] == "}" && @curly_braces[i-1][1] == @curly_braces[i][1]
-      @errors.unshift(@curly_braces[i-1])
+      elsif @curly_braces[i - 1][0] == '}' && @curly_braces[i][0] == '{' && @curly_braces[i - 1][1] != @curly_braces[i][1]
+        @errors.unshift(@curly_braces[i - 1])
+      elsif @curly_braces[i - 1][0] == '{' && @curly_braces[i][0] == '}' && @curly_braces[i - 1][1] == @curly_braces[i][1]
+        @errors.unshift(@curly_braces[i - 1])
       end
     end
   end
 
   def square_brackets_check
-    for i in 1..@square_brackets.length-1 do
-      if @square_brackets[i-1][0] == "]" && @square_brackets[i][0] == "]"
-        @errors.unshift(@square_brackets[i-1])
-      elsif @square_brackets[i-1][0] == "[" && @square_brackets[i][0] == "["
+    for i in 1..@square_brackets.length - 1 do
+      if @square_brackets[i - 1][0] == ']' && @square_brackets[i][0] == ']'
+        @errors.unshift(@square_brackets[i - 1])
+      elsif @square_brackets[i - 1][0] == '[' && @square_brackets[i][0] == '['
         @errors.unshift(@square_brackets[i])
-      elsif @square_brackets[i-1][0] == "]" && @square_brackets[i][0] == "[" && @square_brackets[i-1][1] != @square_brackets[i][1]
-      @errors.unshift(@square_brackets[i-1])
-      elsif @square_brackets[i-1][0] == "[" && @square_brackets[i][0] == "]" && @square_brackets[i-1][1] == @square_brackets[i][1]
-      @errors.unshift(@square_brackets[i-1])
+      elsif @square_brackets[i - 1][0] == ']' && @square_brackets[i][0] == '[' && @square_brackets[i - 1][1] != @square_brackets[i][1]
+        @errors.unshift(@square_brackets[i - 1])
+      elsif @square_brackets[i - 1][0] == '[' && @square_brackets[i][0] == ']' && @square_brackets[i - 1][1] == @square_brackets[i][1]
+        @errors.unshift(@square_brackets[i - 1])
       end
     end
   end
@@ -148,215 +121,27 @@ class ClosingParenthesis
   end
 
   def sorting_errors
-    p @errors
-      array_length = @errors.size
-      return @errors if array_length <= 1    
-      loop do
-        swapped = false
-        (array_length-1).times do |i|
-          if @errors[i][1] > @errors[i+1][1]
-            @errors[i], @errors[i+1] = @errors[i+1], @errors[i]
-            swapped = true
-          end
-        end    
-        break if not swapped
-      end   
-      @errors
-  end
-=begin
-  def trigger_parentheses
-    if @par_open > @par_closed
-      filter_p_open
-    else
-      filter_p_closed
-    end
-  end
+    array_length = @errors.size
+    return @errors if array_length <= 1
 
-  def filter_p_open
-    @parentheses.each do |el|
-      el.length.times do |i, num|
-        if el[i].include?('(')
-          deletion_left = @parentheses.delete(el)
+    loop do
+      swapped = false
+      (array_length - 1).times do |i|
+        if @errors[i][1] > @errors[i + 1][1]
+          @errors[i], @errors[i + 1] = @errors[i + 1], @errors[i]
+          swapped = true
         end
       end
-    end
-  end
-
-  def filter_p_closed
-    @parentheses.each do |el|
-      el.length.times do |i, num|
-        if el[i].include?(')')
-          deletion_right = @parentheses.delete(el)
-        end
-      end
-    end
-  end
-
-  def trigger_curly
-    if @curly_open > @curly_closed
-      filter_curly_open
-    else
-      filter_curly_closed
-    end
-  end
-
-  def filter_curly_open
-    @parentheses.each do |el|
-      el.length.times do |i, num|
-        if el[i].include?('{')
-          deletion_left = @parentheses.delete(el)
-        end
-      end
-    end
-  end
-
-  def filter_curly_closed
-    @parentheses.each_with_index do |el|
-      el.length.times do |i, num|
-        if el[i].include?('}')
-          deletion_right = @parentheses.delete(el)
-        end
-      end
-    end
-  end
-
-  def trigger_square
-    if @square_open > @square_closed
-      filter_square_open
-    else
-      filter_square_closed
-    end
-  end
-
-  def filter_square_open
-    @parentheses.each_with_index do |el|
-      el.length.times do |i, num|
-        if el[i].include?('[')
-          deletion_left = @parentheses.delete(@parentheses[el])
-        end
-      end
-    end
-  end
-
-  def filter_square_closed
-    @parentheses.each_with_index do |el|
-      el.length.times do |i, num|
-        if el[i].include?(']')
-          deletion_right = @parentheses.delete(el)
-        end
-      end
-    end
-  end
-
-
-  def triggerer_of_parentheses
-    unless @parentheses.empty?
-      @parentheses.each do |el|
-        el.length.times do |i|
-          if el[i] == '('
-            elm = ')'
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          elsif el[i] == ')'
-            elm = '('
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          end
-        end
-      end
-    end
-  end
-
-  def triggerer_of_curly
-    unless @curly_braces.empty?
-      @curly_braces.each do |el|
-        el.length.times do |i|
-          if el[i] == '{'
-            elm = '}'
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          elsif el[i] == '}'
-            elm = '{'
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          end
-        end
-      end
-    end
-  end
-
-  def triggerer_of_squares
-    unless @square_brackets.empty?
-      @square_brackets.each do |el|
-        el.length.times do |i|
-          if el[i] == '['
-            elm = ']'
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          elsif el[i] == ']'
-            elm = '['
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          end
-        end
-      end
-    end
-  end
-=end
-  def puts_errors
-    @les_erreurs
-  end
-=begin
-  def parentheses_add(file_path)
-    File.open(file_path).each_with_index do |element, line|
-      element.length.times do |i|
-        @linea = line + 1
-        case element[i]
-        when '('
-          @open_p = @opened.unshift([element[i], @linea.to_s])
-        when ')'
-          @closed_p = @closed.unshift([element[i], @linea.to_s])
-        when '{'
-          @open_curly = @les_erreurs.unshift([element[i], @linea.to_s])
-        when '}'
-          @closed_curly = @closed.unshift([element[i], @linea.to_s])
-        when '['
-          @open_b = @les_erreurs.unshift([element[i], @linea.to_s])
-        when ']'
-          @closed_p = @closed.unshift([element[i], @linea.to_s])
-        end
-      end
-    end
-  end
-=end
-
-=begin
-  def trigger_errors
-    unless @les_erreurs.empty?
-      @les_erreurs.each do |el|
-        el.length.times do |i|
-          if el[i] == '('
-            elm = ')'
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          elsif el[i] == '['
-            elm = ']'
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          elsif el[i] == '{'
-            elm = '}'
-            @errors.unshift("Error, missing #{elm} on line #{el[i + 1]}")
-          end
-        end
-      end
+      break unless swapped
     end
     @errors
   end
 
-
-
-  def seek_and_destroy(element)
-    @les_erreurs.length.times do |j|
-      unless @les_erreurs[0][0].include?(element)
-        @les_erreurs.delete_at(j + 1)
-        break
-      end
-    end
+  def puts_errors
+    @les_erreurs
   end
-=end
-def display_path
-  @file_path
-end
+
+  def display_path
+    @file_path
+  end
 end
